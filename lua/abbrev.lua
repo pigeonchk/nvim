bd445dd7e4de757bb66c804390f7c7caa639a0c2
nihilistcoder <gg.mbrb@gmail.com>
2023-03-10 19:35:28 -0300

    remove some problematic abbreviations

--- lua/abbrev.lua
+++ lua/abbrev.lua
@@ -1,28 +1,12 @@
+local fn = vim.fn
 local iabbrev = require('viml').iabbrev
 local inoremap = require('viml').inoremap
 
 local eat_all_chars = '<C-R>=v:lua.require("abbrev")._eatchar(".")<CR>'
 
-iabbrev {'<silent>', '#!', '#!/usr/bin/'.. eat_all_chars}
-iabbrev {'<silent>', '(', '()<left>'..eat_all_chars}
-iabbrev {'<silent>', '{', '{}<left>'..eat_all_chars}
-iabbrev {'<silent>', '[', '[]<left>'..eat_all_chars}
-iabbrev {'<silent>', '<>', '<><left>'..eat_all_chars}
-iabbrev {'<silent>', '"', '""<left>'..eat_all_chars}
-iabbrev {'<silent>', "'", "''<left>"..eat_all_chars}
-
--- these maps will make the abbreviations above work where it normally wouldn't
-inoremap('(', '<esc>a(')
-inoremap('{', '<esc>a{')
-inoremap('[', '<esc>a[')
-inoremap('<', '<esc>a<')
-inoremap('"', '<esc>a"')
-inoremap('\'', '<esc>a\'')
-
 local M = {
     eat_all_chars = eat_all_chars
 }
-local fn = vim.fn
 
 function M._eatchar(pattern)
     c = fn.getcharstr(0)
