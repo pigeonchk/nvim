f058c45c6985337b2e90e2eb2986e84234bd16d5
nihilistcoder <gg.mbrb@gmail.com>
2023-03-22 16:46:03 -0300

    extended a few options of .project.json

--- lua/project.lua
+++ lua/project.lua
@@ -1,7 +1,12 @@
 local upsearch  = require('utils').upsearch
 
 local valid_options = {
-    makeprg = { type = 'string' }
+    makeprg     = { type = 'string' },
+}
+
+local valid_variables = {
+    builddir    = { type = 'string' },
+    target      = { type = 'string' },
 }
 
 local M = { }
@@ -26,25 +31,39 @@ M.setup_if_project = function()
     local opts = vim.fn.json_decode(lines)
 
     for key, val in pairs(opts) do
-        if not valid_options[key] then
-            if not vim.o[key] then
-                vim.notify('unknown option \''..key..'\' found while parsing \'.project.json\'',
+        if valid_options[key] then
+            if type(val) ~= valid_options[key].type then
+                vim.notify(
+                    {'option \''..key..'\' has invalid type in \'.project.json\'.',
+                     'expected \''..valid_options[key].type..'\', but got \''..type(val)..'\''},
                     vim.log.levels.ERROR, {title = NOTIFY_TITLE})
             else
-                vim.notify(
-                    'option \''..key..'\' not supported found while parsing \'.project.json\'',
-                    vim.log.levels.ERROR, {title = NOTIFY_TITLE})
+                vim.go[key] = val
             end
+        elseif valid_variables[key] then
+            vim.g['project_'..key] = val
+        elseif not vim.tbl_get(vim.go, key) then
+                vim.notify('unknown option \''..key..'\' found while parsing \'.project.json\'',
+                    vim.log.levels.ERROR, {title = NOTIFY_TITLE})
+        else
+            vim.notify('option \''..key..'\' not supported found while parsing \'.project.json\'',
+                vim.log.levels.ERROR, {title = NOTIFY_TITLE})
         end
 
-        if type(val) ~= valid_options[key].type then
-            vim.notify(
-                {'option \''..key..'\' has invalid type in \'.project.json\'.',
-                 'expected \''..valid_options[key].type..'\', but got \''..type(val)..'\''},
-                vim.log.levels.ERROR, {title = NOTIFY_TITLE})
+    end
+
+    if vim.g.project_target and vim.g.project_builddir then
+        local builddir
+        if vim.g.project_builddir[1] == '/' then
+            builddir = vim.g.project_builddir
         else
-            vim.go[key] = val
+            builddir = vim.fn.getcwd() ..'/'..vim.g.project_builddir
         end
+
+        local cmd = builddir .. '/'.. vim.g.project_target
+        vim.api.nvim_create_user_command('Run', function(tbl)
+            vim.cmd('!'..cmd..' '..tbl.args)
+        end, {force = true, nargs='*'})
     end
 end
 
