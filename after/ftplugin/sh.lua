08be5b36d8316c93a9e78a60de4701f101230b62
pigeonchk <gabrielmanoel13@gmail.com>
2023-05-11 16:51:14 -0300

    add comment leader for sh

--- after/ftplugin/sh.lua
+++ after/ftplugin/sh.lua
@@ -2,4 +2,5 @@ require('ftplugin.create')('sh', function()
     -- indentation
     vim.bo.cindent = true
     vim.bo.cinoptions = vim.o.cinoptions .. '#1'
+    vim.b.license_comment_leader = '#'
 end)
